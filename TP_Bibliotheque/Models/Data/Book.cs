62ced9430edac287e70c10bd615158aab648fda9
Sylvain Metayer <sylvain_metayer@outlook.fr>
2017-06-09 23:36:46 +0200

    Update About page, Remove useless test & update Book Model

--- TP_Bibliotheque/Models/Data/Book.cs
+++ TP_Bibliotheque/Models/Data/Book.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TP_Bibliotheque.Models.Data
 {
     public class Book
     {
+        public static int IdMax { get; set; }
+        [Required]
+        public int Id { get; set; }
         public string Title { get; set; }
-        public Author author { get; set; }
+        public Author Author { get; set; }
         public string Edition { get; set; }
-        public DateTime publicationDate { get; set; }
-        public int availableQuantity { get; set; }
-        public string thematic { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int AvailableQuantity { get; set; }
+        public string Thematic { get; set; }
 
     }
 }
